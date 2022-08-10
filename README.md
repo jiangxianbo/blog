5a14944489fff1c1ded2e65884a5c35a939cbdb1
jiangxianbo <jxb42359@163.com>
2022-08-10 09:44:15 +0800

    README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # blog
+
+## `目录结构`
+
+```
+- model 「实体」
+- dao 「数据层」
+- service 「业务逻辑层」
+- controller 「控制器」
+- static 「前端静态资源」
+	- css 「css 文件」
+	- img 「图片」
+	- js 「js 文件」
+- utils 「工具类」
+- view 「html模板」
+- main.go 「主函数」
+```
+
+***
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+func GetValue() interface{} {
+	return 1
+}
+
+func main() {
+	i := GetValue()
+	switch i.(type) {
+	case int:
+		println("int")
+	case string:
+		println("string")
+	case interface{}:
+		println("interface")
+	default:
+		println("unknown")
+	}
+}
